877bc8f815875c4ba0bcb51c8a46860f66f85b6f
jmjubaer <jmjubaer3927@gmail.com>
2025-07-04 10:16:05 +0600

    add simple navbar

--- src/components/shered/Navbar.tsx
+++ src/components/shered/Navbar.tsx
@@ -1,12 +1,33 @@
-import { Link } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div>
-            <Link to={"/"}>Q Shop</Link>
+        <div className='container py-3 flex items-center justify-between'>
+            <Link to='/' className='flex items-center space-x-2'>
+                <div className='w-9 h-9 gradient_bg rounded-lg flex items-center justify-center'>
+                    <span className='text-white font-bold text-lg'>Q</span>
+                </div>
+                <span className='text-xl font-bold text-gray-900'>Q Shop</span>
+            </Link>
             <nav>
-                <ul>
-                    <li></li>
+                <ul className='flex items-center gap-5'>
+                    <li>
+                        <NavLink
+                            to={"/"}
+                            className={({ isActive }) =>
+                                isActive
+                                    ? "text-blue-500 border-b-2"
+                                    : "text-gray-500"
+                            }>
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <button className='p-3 hover:bg-gray-100 rounded-md text-xl cursor-pointer'>
+                            <FaShoppingCart />
+                        </button>
+                    </li>
                 </ul>
             </nav>
         </div>
